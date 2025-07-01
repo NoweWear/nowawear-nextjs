bfe03d34f7df2a6781fdf55c7d49346ea2b39c75
NoweWear <isletmehesabim2025@gmail.com>
2025-07-01 17:16:41 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>NoweWear'a Hoş Geldiniz 👕</h1>
+      <p>Printify API ile otomatik ürün listeleme yakında burada!</p>
+    </div>
+  );
+  }
