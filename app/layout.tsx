d6e8477ceb39cefbf759376699c5e51c9027bcec
NoweWear <isletmehesabim2025@gmail.com>
2025-07-01 23:59:55 +0300

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "NoweWear",
+  description: "Automatic product listing with Printify API",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
