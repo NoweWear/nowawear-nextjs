e9603e3e761d208fd4dd87758b5f01c38c1ecb46
NoweWear <isletmehesabim2025@gmail.com>
2025-07-01 20:23:51 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to NoweWear 👕</h1>
+      <p>Our custom-made apparel is coming soon. Stay tuned!</p>
+    </main>
+  );
+}
