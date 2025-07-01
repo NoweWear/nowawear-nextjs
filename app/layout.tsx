b60193313d9492400c5befb9dcc9a0c5913074f8
NoweWear <isletmehesabim2025@gmail.com>
2025-07-01 22:04:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+
 export const metadata = {
   title: "NoweWear",
   description: "Welcome to NoweWear custom apparel store!",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
