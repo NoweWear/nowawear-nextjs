84773766638fc02d11108713df9b4d584f7f38ed
NoweWear <isletmehesabim2025@gmail.com>
2025-07-01 23:44:00 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-// app/layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head />
+    <html>
       <body>{children}</body>
     </html>
   );
